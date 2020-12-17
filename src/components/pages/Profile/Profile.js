f8f213e357a88ccda6d14d2713486d57907a4143
TATIANA Toro <tatianatorog@gmail.com>
2020-12-16 22:50:02 -0500

    updating readme

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,17 +1,12 @@
-import React, { useState, useContext  } from "react";
+import React, { useState, useContext } from "react";
 import UserHeader from "../../UserHeader/UserHeader";
 import UserInput from "../../fields/UserInput";
-import "./Profile.scss"  
-import {AppContext} from "../../../utils/useContext"
-
-
+import "./Profile.scss";
+import { AppContext } from "../../../utils/useContext";
 
 export default function Profile() {
-  const [states, setstates] = useState(false);
-  const {user } = useContext(AppContext);
-  const [email, setEmail] = useState(user?.user_email);
-  
-  
+  const { user } = useContext(AppContext);
+
   return (
     <>
       <div className="user-settings">
@@ -19,14 +14,11 @@ export default function Profile() {
           <>
             <UserHeader user={user} />
             <p></p>
-            <UserInput user={user}/>
-            {/* <UserInput email={user?.user_email} changeEmail={changeEmail}  timezone={timezone} changeTimezone={changeTimezone} theme={theme} changeTheme={changeTheme} language={language} changeLanguage={changeLanguage} onClick={updateUser}></UserInput> */}
-            {/* <Features user={user.ENABLED_FEATURES}></Features> */}
+            <UserInput user={user} />
+           
           </>
         ) : null}
       </div>
     </>
   );
 }
-
-
